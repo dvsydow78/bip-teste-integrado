5de3981a8a8eac82478739b2d87fd49b5f9c50a2
Daniel von Sydow <dvsydow78@gmail.com>
2026-03-05 15:48:07 -0300

    teste

--- frontend/bip-frontend/src/app/features/pages/beneficio/remover-beneficio/remover-beneficio.spec.ts
+++ frontend/bip-frontend/src/app/features/pages/beneficio/remover-beneficio/remover-beneficio.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-<<<<<<< HEAD
 
 import { RemoverBeneficio } from './remover-beneficio';
 
@@ -15,36 +14,9 @@ describe('RemoverBeneficio', () => {
     fixture = TestBed.createComponent(RemoverBeneficio);
     component = fixture.componentInstance;
     await fixture.whenStable();
-=======
-import { RemoverBeneficioComponent } from './remover-beneficio';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-
-describe('RemoverBeneficio', () => {
-  let component: RemoverBeneficioComponent;
-  let fixture: ComponentFixture<RemoverBeneficioComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RemoverBeneficioComponent, NoopAnimationsModule],
-      providers: [
-        provideHttpClient(),
-        provideRouter([]),
-      ],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(RemoverBeneficioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
->>>>>>> dad4441dbaa060a6db8ee5b2e5521c7ecfb61a3e
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> dad4441dbaa060a6db8ee5b2e5521c7ecfb61a3e
